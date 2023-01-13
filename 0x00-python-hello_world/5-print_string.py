3a68d86303be282420f58b1849b2d559cba3deb5
Dolapo Abdul <kellyworks91@gmail.com>
2023-01-13 02:57:52 +0000

    Python Hello world

---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Holberton School"
+thrice = str * 3
+print(f"{thrice}")
+print(str[:9])
---
+++ 0x00-python-hello_world/README.md
@@ -0,0 +1,2 @@
+this directory contains should contain all the solutions to the python-
+hello world exercises
