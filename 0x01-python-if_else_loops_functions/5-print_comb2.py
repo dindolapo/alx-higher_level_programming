88dbc117f275a41e6d030520d56f1910f3187612
Dolapo Abdul <kellyworks91@gmail.com>
2023-01-24 13:40:44 +0000

    correction made again

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
-print(", ".join(f"{i:0<2}" for i in range(100)))
+for i in range(100):
+    if i > 98:
+        print("{:d}".format(i), end='\n')
+    else:
+        print("{:0>2}".format(i), end=', ')
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
     for a in str:
         if ord(a) > 96 and ord(a) < 123:
             a = chr(ord(a) - 32)
-        print(a, end='')
+        print("{}".format(a), end='')
     print()
